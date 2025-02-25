148726bc81632939ad85c6aa719551c700d64e85
VvencesS <ducsoan99@gmail.com>
2025-02-26 00:48:24 +0700

    Viết Custom Gateway Filter Để Authentication Và Authorization

--- apigateway/src/main/java/com/ltfullstack/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/ltfullstack/apigateway/ApigatewayApplication.java
@@ -3,6 +3,11 @@ package com.ltfullstack.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +17,9 @@ public class ApigatewayApplication {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
 
+	@Bean
+	public KeyResolver keyResolver(){
+		// dùng địa chỉ làm key
+		return exchange -> Mono.just(Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress());
+	}
 }
