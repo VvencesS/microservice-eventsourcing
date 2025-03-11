34a91765a1407c97db8f14da13bd251ac88b1637
VvencesS <ducsoan99@gmail.com>
2025-03-11 22:26:53 +0700

    Tích Hợp Apache Kafka Vào Notification Service

--- booksevice/src/main/java/com/ltfullstack/booksevice/query/controller/BookQueryController.java
+++ booksevice/src/main/java/com/ltfullstack/booksevice/query/controller/BookQueryController.java
@@ -3,13 +3,11 @@ package com.ltfullstack.booksevice.query.controller;
 import com.ltfullstack.booksevice.query.model.BookResponseModel;
 import com.ltfullstack.booksevice.query.queries.GetAllBooksQuery;
 import com.ltfullstack.booksevice.query.queries.GetBookDetailQuery;
+import com.ltfullstack.commonservice.sevices.KafkaService;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +16,9 @@ import java.util.List;
 public class BookQueryController {
     @Autowired
     private QueryGateway queryGateway;
+
+    @Autowired
+    private KafkaService kafkaService;
 
     @GetMapping
     public List<BookResponseModel> getAllBooks(){
@@ -29,5 +30,10 @@ public class BookQueryController {
     public BookResponseModel getBookById(@PathVariable String bookId){
         GetBookDetailQuery query = new GetBookDetailQuery(bookId);
         return queryGateway.query(query, ResponseTypes.instanceOf(BookResponseModel.class)).join();
+    }
+
+    @PostMapping("/sendMessage")
+    public void sendMessage(@RequestBody String message){
+        kafkaService.sendMessage("test", message);
     }
 }
